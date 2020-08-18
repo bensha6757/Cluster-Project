4b5b1071056e0a7b510a5531d228ea4e90cd6568
Gal Bezalel <galbezalel@gmail.com>
2020-08-19 00:06:10 +0300

    Add new comments

--- algorithm.c
+++ algorithm.c
@@ -5,7 +5,7 @@
  *      Author: ть
  */
 
-#include "modMat.c"
+#include "modMat.h"
 #define G1 1
 #define G2 -1
 typedef enum message_t {
@@ -14,35 +14,29 @@ typedef enum message_t {
 } MESSAGE;
 
 typedef int* s_vector;
+typedef double* vector;
 
-
-void powerIteration(vector ret, spmat *mat, vector v, dimension d){
+void powerIteration(vector ret, modMat *B, vector v){
 	unsigned int i;
 	double nrm;
-	mat->mult(mat,v,ret);
-	nrm=norm(ret,d);
-	for (i=0; i<d; i++, ret++)
+	B->mult(B,B->g,v,ret);
+	nrm=norm(ret,B->n);
+	for (i=0; i<B->n; i++, ret++)
 		*ret/=nrm;
-	#ifdef DEBUG
-		printf("main: Iteration done\n");
-	#endif
 }
 
 /*Compute Modularity of B[g]_hat: 0.5 * s^T * B[g]_hat * s */
-double getModularity(ModMat *B,Subgroup g, s_vector s){
+double getModularity(modMat *B, Subgroup g, s_vector s){
 	double Q;
-	int *res1=(*int)malloc(B->n*sizeof(int));
-	double *res2=(*double)malloc(B->n*sizeof(double));
-	B->A->mult(B->A, g, s, res1);
-	B->K->mult(B->K, g, s, res2); /* consider M */
-	Q = 0.5 *(dotProd(res1,s,B->n)-dotProd(res2,s,B->n));
-	free(res1);
-	free(res2);
+	double *Bs=(double*)malloc(B->n*sizeof(double));
+	B->mult(B, B->g, s, Bs);
+	Q = 0.5 * dotProd(Bs,s,B->n);
+	free(Bs);
 	return Q;
 }
 
 /*Optimize a division encoded by {-1,1} vector s by moving a vertex to other group and ascending modularity Q */
-void optimizeDivision(ModMat *B, s_vector s){
+void optimizeDivision(modMat *B, s_vector s){
 	int *entry=s;
 	double Qprev,Qnext;
 	Qprev=B->getModularity(B,B->g,s);
