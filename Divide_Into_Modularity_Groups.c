982991bffc4bf38e288919771cd3f8ae85028910
Gal Bezalel <galbezalel@gmail.com>
2020-08-31 02:30:48 +0300

    add DEBUG prints

--- Divide_Into_Modularity_Groups.c
+++ Divide_Into_Modularity_Groups.c
@@ -6,7 +6,6 @@ void add_to_stacks(Stack* P, Stack* O, num sizeG, num sizeG1, num sizeG2, Subgro
 			push(O, g, sizeG);
 	}
 	else{
-		free(g); /* g has been divided into 2 groups, so now it has no use anymore */
 		if (sizeG1 == 1){
 			push(O, g1, sizeG1);
 		}
@@ -21,6 +20,17 @@ void add_to_stacks(Stack* P, Stack* O, num sizeG, num sizeG1, num sizeG2, Subgro
 		}
 	}
 }
+
+
+void printG(Subgroup g, int n){
+	Subgroup p;
+	for (p = g; p < g + n ; p++){
+		printf("%d, ", *p);
+	}
+	printf("\n");
+}
+
+
 /* Algorithm 3*/
 Stack* div_into_mod_groups(modMat* B, Subgroup g, num sizeG){
 	Stack* P = (Stack*)malloc(sizeof(Stack)), *O = (Stack*)malloc(sizeof(Stack));
@@ -35,8 +45,16 @@ Stack* div_into_mod_groups(modMat* B, Subgroup g, num sizeG){
 	init(O); /* initializing O to be empty */
 	push(P,g,sizeG); /* starting with a trivial division into one group */
 	while (!isEmpty(P)){
+		#ifdef DEBUG
+		printf("Alg 3  - NEW ITER\n");
+		#endif
 		g = pop(P, &sizeG); /* g would be g1 or g2 from some previous iteration */
 		div_into_two(B, g, sizeG, &g1, &g2, &sizeG1, &sizeG2); /* calling Algorithm 2, (g1,g2,sizeG1,sizeG2) will be populate inside the function with the result of the division */
+		#ifdef DEBUG
+		printf("Alg 2 completed - division:\n");
+		printG(g1,sizeG1);
+		printG(g2,sizeG2);
+		#endif
 		add_to_stacks(P, O, sizeG, sizeG1, sizeG2, g1, g2, g);
 	}
 	free(P);
