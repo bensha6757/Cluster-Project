4b3b568ef1f39c5ff13460d5fe048c441121cc51
Gal Bezalel <galbezalel@gmail.com>
2020-09-02 23:33:33 +0300

    add exit codes enums

--- io_mem_errors.h
+++ io_mem_errors.h
@@ -9,10 +9,13 @@
     if (!(expression)){                                                                      \
         printf("\n%s%s, %s", "Verification Failed: ", #expression, stringFromError(error));   \
         printf("\n");                                                                          \
-        exit(1);                                                                               \
+        exit(error);                                                                               \
     }                                                                \
 
 typedef enum error_t {
+    SUCCESS,
+    MISSING_ARG_ERROR,
+    FILE_NOT_FOUND_ERROR,
 	FILE_READ_ERROR,
 	FILE_WRITE_ERROR,
 	MEM_ALLOC_ERROR,
@@ -23,7 +26,17 @@ typedef enum error_t {
 
 static const char* stringFromError(enum error_t e)
 {
-    static const char *strings[] = { "Error while reading the file", "Error while writing to file", "Memory allocation error", "Infinite loop error", "Out of bounds error", "Null pointer error"};
+    static const char *strings[] = { 
+                                    "Program Completed Successfully",
+                                    "Missing program argument error",
+                                    "Error while opening file: not found or corrupted",
+                                    "Error while reading the file", 
+                                    "Error while writing to file", 
+                                    "Memory allocation error", 
+                                    "Infinite loop error", 
+                                    "Out of bounds error", 
+                                    "Null pointer error", 
+                                    };
     return strings[e];
 }
 
